6a49e90078c50e7b4eea8f968216ba5802cb8109
ranjanaau <>
2021-05-26 11:41:55 +0000

    check whether odd or even

--- Problem_3.c
+++ Problem_3.c
@@ -7,7 +7,10 @@ int main() {
     scanf("%d", &number);
 
     // Write your code here
-    
+    if(number%2==0)
+    printf("%d is even\n",number);
+    else
+    printf("%d is odd\n",number);
     
    
     return 0;
